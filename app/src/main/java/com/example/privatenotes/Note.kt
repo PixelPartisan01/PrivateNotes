2dee5d641bc8546e96549a1a408192aa8759f5ec
Szasa40 <60932792+Szasa40@users.noreply.github.com>
2021-11-01 16:50:51 +0100

    feat: background

--- app/src/main/java/com/example/privatenotes/Note.kt
+++ app/src/main/java/com/example/privatenotes/Note.kt
@@ -1,11 +1,14 @@
 package com.example.privatenotes
 
 import android.content.Context
+import android.graphics.drawable.AnimationDrawable
 import android.icu.util.Output
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.io.*
 import java.lang.StringBuilder
 
@@ -14,9 +17,19 @@ class Note : AppCompatActivity()
     lateinit var textArea: EditText
 
     override fun onCreate(savedInstanceState: Bundle?)
-    {
+    {        supportActionBar?.hide()
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
+
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.layoutAnimatedBackground)
+        val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
 
         val path = this.getExternalFilesDir(null)
         val folder = File(path, "Privat Note")
