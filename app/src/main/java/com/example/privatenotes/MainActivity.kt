e2f553a366f824174ca0de51e752ae0c9d761095
Szasa40 <60932792+Szasa40@users.noreply.github.com>
2021-11-01 08:52:15 +0100

    Create Project

--- app/src/main/java/com/example/privatenotes/MainActivity.kt
+++ app/src/main/java/com/example/privatenotes/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.privatenotes
 
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricPrompt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
